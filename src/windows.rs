b0f768f0e780bbed3ab6432b7ce3db310cc19183
PaulLagabbe <plagabbe@gmail.com>
2024-03-12 10:38:05 +0100

    Enhance window object

--- src/windows.rs
+++ src/windows.rs
@@ -8,50 +8,58 @@ use std::f64::consts::PI;
 /// Window object iterable
 #[derive(Clone)]
 pub struct Window {
-	size: usize,
 	overlap: usize,
-	function: fn(usize, usize) -> f64,
+	vector: Vec<f64>,
 }
 
 
 impl Window {
 
-	/// constructor method, create a window object
+	/// constructor method, create a rectangle window object
 	pub fn rectangle(duration: f64, overlap_time: f64, fs: f64) -> Self {
 
 		let size: usize = (duration * fs).round() as usize;
 		let overlap: usize = (overlap_time * fs).round() as usize;
 		assert!(size > overlap);
-
+		// make window vector
+		let mut vector: Vec<f64> = Vec::new();
+		for i in 0..size {
+			vector.push(1.);
+		}
 		Window {
-			size,
 			overlap,
-			function: rectangle,
+			vector,
 		}
 
 	}
-	/// constructor method, create a window object
+	/// constructor method, create a Hann window object
 	pub fn hann(duration: f64, overlap_time: f64, fs: f64) -> Self {
 
 		let size: usize = (duration * fs).round() as usize;
 		let overlap: usize = (overlap_time * fs).round() as usize;
 		assert!(size > overlap);
+		// make window vector
+		let mut vector: Vec<f64> = Vec::new();
+		let mut phi: f64;
+		for i in 0..size {
+			phi =  2. * PI * (i as f64) / (size as f64);
+			vector.push(0.5 * (1. - phi.cos()));
+		}
 
 		Window {
-			size,
 			overlap,
-			function: hann,
+			vector,
 		}
 
 	}
 	/// Compute the number of window that can be computed on the data vector given in parameter
 	pub fn nb_fft(&self, data_size: usize) -> usize {
-		(data_size - self.size) / (self.size - self.overlap) + 1
+		(data_size - self.vector.len()) / (self.vector.len() - self.overlap) + 1
 	}
 
 	/// Get size of the window in number of points
 	pub fn get_size(&self) -> usize {
-		self.size
+		self.vector.len()
 	}
 
 	/// return a slive of the parameter data vector that has been windowed
@@ -61,11 +69,11 @@ impl Window {
 
 		// initialize output vector
 		let mut output: Vec<Complex<f64>> = Vec::new();
-		let start: usize = k * (self.size - self.overlap);
-		for i in 0..self.size {
+		let start: usize = k * (self.vector.len() - self.overlap);
+		for i in 0..self.vector.len() {
 
 			output.push(Complex{
-				re:	data[i+start] * (self.function)(self.size, i),
+				re:	data[i+start] * self.vector[i],
 				im: 0.});
 		}
 
@@ -76,23 +84,11 @@ impl Window {
 	pub fn get_norm_factor(&self) -> f64 {
 
         let mut alpha: f64 = 0.;
-        for i in 0..self.size {
-            alpha += (self.function)(self.size, i).powi(2);
+        for i in 0..self.vector.len() {
+            alpha += self.vector[i].powi(2);
         };
 		alpha
 	}
-}
-
-/* --------------------------------------------------------------------------------------------- *
- * Window function
- * --------------------------------------------------------------------------------------------- */
-fn hann(n: usize, k: usize) -> f64 {
-	let phi: f64 = 2. * PI * (k as f64) / (n as f64);
-	0.5 * (1. - phi.cos())
-}
-
-fn rectangle(_n: usize, _k: usize) -> f64 {
-	1.
 }
 
 	
